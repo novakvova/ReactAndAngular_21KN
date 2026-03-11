18d9da619e1868016c4d754f20dd13e5931d0350
novakvova <novakvova@gmail.com>
2026-03-11 09:46:38 +0200

    Working cropper js

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -6,7 +6,8 @@ import MainLayout from "./componetns/layouts/MainLayout";
 import RegisterPage from "./pages/RegisterPage.tsx";
 import TestPage from "./pages/TestPage.tsx";
 import ProfilePage from "./pages/ProfilePage.tsx";
-import 'react-cropper/node_modules/cropperjs/dist/cropper.css';
+// import 'react-cropper/node_modules/cropperjs/dist/cropper.css';
+import "cropperjs/dist/cropper.css";
 
 function App() {
 
