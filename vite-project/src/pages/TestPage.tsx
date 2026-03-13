4771264ff401a2daba095e18cdce5a201b5ae68d
novakvova <novakvova@gmail.com>
2026-03-13 10:57:47 +0200

    finish TestPage with Cropper

--- vite-project/src/pages/TestPage.tsx
+++ vite-project/src/pages/TestPage.tsx
@@ -1,11 +1,12 @@
 import cat from '../assets/cat.webp'; // Relative path to the image file
-import {useEffect, useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 import Cropper from "cropperjs";
 
 const TestPage = () => {
     //Посилання на фото, яке буде працювати у Cropper
     const imgRef = useRef<HTMLImageElement | null>(null);
     const cropperRef = useRef<Cropper | null>(null);
+    const [image, setImage] = useState<string | null>(null);
 
     useEffect(() => {
         // const Cropper = window.Cropper;
@@ -21,12 +22,27 @@ const TestPage = () => {
         };
     },[]);
 
+    const handleCrop = () => {
+        // перевіряємо чи існує посилання на Cropper
+        if (!cropperRef.current) return;
+        // дістаємо cropper за посиланням
+        const cropper = cropperRef.current;
+        // отримуємо зображення у base64
+        const base64 = cropper.getCroppedCanvas().toDataURL();
+        // записуємо зображення
+        setImage(base64);
+    }
+
     return (
         <div>
             <img src={cat}
                  alt="Фото для редагування кота"
                  ref={imgRef}
             />
+            <button onClick={handleCrop} className={'my-4 bg-blue-500 text-white font-bold py-2 px-4 rounded cursor-pointer'}>
+                Обрізати зображення
+            </button>
+            {image && <img src={image} alt={"Обрізане фото"} className={'w-64'}/> }
         </div>
     )
 }
