652216e691fb12b2e7d303d2c8859988af7a9ded
novakvova <novakvova@gmail.com>
2026-05-06 11:54:20 +0300

    Is good register

--- vite-queryrtk/src/services/apiAccount.ts
+++ vite-queryrtk/src/services/apiAccount.ts
@@ -2,17 +2,24 @@ import {createApi} from "@reduxjs/toolkit/query/react";
 import {createAWSQuery} from "../util/createAWSQuery.ts";
 import type {IRegister} from "../types/account/IRegister.ts";
 import type {IRegisterResponse} from "../types/account/IRegisterResponse.ts";
+import {serialize} from "object-to-formdata";
 
 export const apiAccount = createApi({
     reducerPath: 'account',
     baseQuery: createAWSQuery("api/account"),
     endpoints: (builder) => ({
         register: builder.mutation<IRegisterResponse, IRegister>({
-            query: (body) => ({
-                url: "register",
-                method: "POST",
-                body
-            })
+            query: (body) =>
+            {
+                console.log("Submit Data Query", body);
+                // const form = serialize(body);
+                // console.log("is form DATA", form instanceof FormData); // має бути true
+                return {
+                    url: "register",
+                    method: "POST",
+                    body
+                }
+            }
         })
     })
 });
